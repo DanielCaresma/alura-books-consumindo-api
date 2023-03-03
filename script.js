f45e8c004dfdd39c080f90c6625b9479a6ddd919
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-03-03 15:05:37 -0300

    Imprimindo informações na tela

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 async function buscaEnderecos(cep) {
+    let menssagemErro = document.getElementById('erro')
+    menssagemErro.innerHTML = ""
     try {
         var consultaCep =  await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCep.json()
         if (consultaCEPConvertida.erro) {
             throw Error ('CEP não encontrado')
         }
-        console.log(consultaCEPConvertida)
-        
+        const logradouro = document.getElementById('endereco')
+        const bairro = document.getElementById('bairro')
+        const cidade = document.getElementById('cidade')
+        const estado = document.getElementById('estado')
+
+        logradouro.value = consultaCEPConvertida.logradouro
+        bairro.value = consultaCEPConvertida.bairro
+        cidade.value = consultaCEPConvertida.localidade
+        estado.value = consultaCEPConvertida.uf
+
+        console.log(consultaCEPConvertida)        
         return consultaCEPConvertida
     } catch (erro) {
+        menssagemErro.innerHTML = '<p> CEP inválido. Tente novamente! </p>'
         console.log(erro)
     }
 }
 
-let ceps = ['01001000', '01001001']
-let conjuntoCeps = ceps.map(valor => buscaEnderecos(valor))
-console.log(conjuntoCeps)
-Promise.all(conjuntoCeps).then(response => console.log(response))
+let cep = document.getElementById('cep')
+cep.addEventListener('focusout', () => buscaEnderecos(cep.value))
